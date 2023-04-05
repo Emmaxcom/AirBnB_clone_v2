5febc86b75070af8443b797be10005a60aae51d6
Emmaxcom <emmaxcom@yahoo.com>
2023-04-05 13:38:17 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
-"""
-script that distributes archive to webservers
-"""
-import os.path
-from fabric.api import *
-from fabric.operations import run, put, sudo
-env.hosts = ['52.90.98.156', '52.207.85.204']
+# makes tgz archive
+import tarfile
+from fabric.api import local, env, run, put
+from datetime import datetime
+import os
+
+
+env.hosts = ['35.196.73.169', '35.231.71.122']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/holberton'
+
+
+def do_pack():
+    # packs file
+    try:
+        name = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S")
+        local('mkdir -p versions')
+        local("tar -cvzf versions/{}.tgz {}".format(
+            name, "web_static/"))
+        size = os.path.getsize("./versions/{}.tgz".format(name))
+        print("web_static packed: versions/{}.tgz -> {}Bytes".format(
+            name, size))
+        return "versions/{}.tgz".format(name)
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """ deploy """
-    if (os.path.isfile(archive_path) is False):
-        return False
-
+    fd = archive_path.split('/')[1]
     try:
-        new_comp = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(new_comp, new_folder))
-        run("sudo rm /tmp/{}".format(new_comp))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        put(archive_path, '/tmp/{}'.format(fd))
+        run('mkdir -p /data/web_static/releases/{}'.format(fd))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(fd, fd))
+        run('rm /tmp/{}'.format(fd))
+        run('mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}/'.format(fd, fd))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(fd))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/\
+        /data/web_static/current'.format(fd))
+        print("New version deployed!")
         return True
     except:
+        print("New version not deployed...")
         return False
